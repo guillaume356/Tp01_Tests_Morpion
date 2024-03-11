bb791d92c9c0d3c1b7ce7216e15537f234cccaaa
Guillaume KLEIN <kleinguillaume005@gmail.com>
2024-03-11 15:04:48 +0100

    gitignore

---
+++ MorpionTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+using MorpionApp;
+namespace MorpionTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal();
+        }
+    }
+}
