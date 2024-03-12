5fd3a354f241ce0527742db546fa0d23b10f0609
Guillaume KLEIN <kleinguillaume005@gmail.com>
2024-03-12 09:53:43 +0100

    Création feuilles tests Puissance4 et ajout de tests verifVictoire pour le Puissance4

---
+++ MorpionTest/Puissance4TestFixture.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MorpionApp;
+using Xunit;
+
+namespace MorpionTest
+{
+    public class Puissance4TestFixture
+    {
+        public PuissanceQuatre puissanceQuatre;
+
+        public Puissance4TestFixture()
+        {
+            puissanceQuatre = new PuissanceQuatre();
+        }
+
+        public void ResetGrille()
+        {
+            puissanceQuatre.grille = new char[4, 7];
+        }
+
+        public void SetColonneVictorieuse(char joueur, int colonne)
+        {
+            for (int ligne = 0; ligne < 4; ligne++)
+            {
+                puissanceQuatre.grille[ligne, colonne] = joueur;
+            }
+        }
+
+        public void SetLigneVictorieuse(char joueur, int ligne)
+        {
+            for (int colonne = 0; colonne < 4; colonne++) 
+            {
+                puissanceQuatre.grille[ligne, colonne] = joueur;
+            }
+        }
+
+        public void SetDiagonaleVictorieuse(char joueur)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                puissanceQuatre.grille[i, i] = joueur;
+            }
+        }
+
+        public void SetDiagonaleInverseVictorieuse(char joueur)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                puissanceQuatre.grille[3 - i, i] = joueur;
+            }
+        }
+    }
+}
---
+++ MorpionTest/Puissance4Tests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MorpionTest
+{
+    public class Puissance4Tests : IClassFixture<Puissance4TestFixture>
+    {
+        private readonly Puissance4TestFixture _fixture;
+
+        public Puissance4Tests(Puissance4TestFixture fixture)
+        {
+            _fixture = fixture;
+            _fixture.ResetGrille();
+        }
+
+        [Theory]
+        [InlineData('X', 0)] 
+        [InlineData('O', 1)] 
+        public void VerifVictoire_Verticale(char joueur, int colonne)
+        {
+            _fixture.ResetGrille();
+            _fixture.SetColonneVictorieuse(joueur, colonne);
+            bool victoire = _fixture.puissanceQuatre.verifVictoire(joueur);
+            Assert.True(victoire, $"Le joueur {joueur} devrait avoir gagné verticalement dans la colonne {colonne + 1}.");
+        }
+
+        [Theory]
+        [InlineData('X', 0)] 
+        [InlineData('O', 1)] 
+        public void VerifVictoire_Horizontale(char joueur, int ligne)
+        {
+            _fixture.ResetGrille();
+            _fixture.SetLigneVictorieuse(joueur, ligne);
+            bool victoire = _fixture.puissanceQuatre.verifVictoire(joueur);
+            Assert.True(victoire, $"Le joueur {joueur} devrait avoir gagné horizontalement dans la ligne {ligne + 1}.");
+        }
+
+        [Theory]
+        [InlineData('X')]
+        [InlineData('O')]
+        public void VerifVictoire_DiagonalePrincipale(char joueur)
+        {
+            _fixture.ResetGrille();
+            _fixture.SetDiagonaleVictorieuse(joueur);
+            bool victoire = _fixture.puissanceQuatre.verifVictoire(joueur);
+            Assert.True(victoire, $"Le joueur {joueur} devrait avoir gagné sur la diagonale principale.");
+        }
+
+        [Theory]
+        [InlineData('X')]
+        [InlineData('O')]
+        public void VerifVictoire_DiagonaleInverse(char joueur)
+        {
+            _fixture.ResetGrille();
+            _fixture.SetDiagonaleInverseVictorieuse(joueur);
+            bool victoire = _fixture.puissanceQuatre.verifVictoire(joueur);
+            Assert.True(victoire, $"Le joueur {joueur} devrait avoir gagné sur la diagonale inverse.");
+        }
+
+
+    }
+}
