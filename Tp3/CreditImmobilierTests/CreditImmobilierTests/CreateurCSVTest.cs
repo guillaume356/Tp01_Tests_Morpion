4d176f05ec0d0d294088e15a6da4b310941ad70d
Guillaume KLEIN <kleinguillaume005@gmail.com>
2024-04-10 00:01:16 +0200

    final push

--- Tp3/CreditImmobilierTests/CreditImmobilierTests/CreateurCSVTest.cs
+++ Tp3/CreditImmobilierTests/CreditImmobilierTests/CreateurCSVTest.cs
@@ -14,22 +14,25 @@ namespace CreditImmobilier.Tests
             var fakeFileSystem = new FileSystemFake();
             var createurCSV = new CreateurCSV(fakeFileSystem);
 
-            double coutTotalInitial = 500.0;
+            double coutTotalInitial = 500.0; 
             var mensualites = new List<Mensualite>
             {
                 new Mensualite(1, 200.0, 800.0, 50.0, 250.0),
                 new Mensualite(2, 200.0, 600.0, 45.0, 245.0)
             };
             string filePath = "test.csv";
-
             StringBuilder expectedContent = new StringBuilder();
             expectedContent.AppendLine($"Cout total du credit;{coutTotalInitial.ToString("F2", CultureInfo.InvariantCulture)}");
             expectedContent.AppendLine("Numero;Capital Rembourse;Capital Restant Du;Amortissement;Interet;Mensualite;Cout Total");
+
             double capitalRembourseCumulatif = 0;
+            double coutTotal = coutTotalInitial; 
             foreach (var mensualite in mensualites)
             {
                 capitalRembourseCumulatif += mensualite.CapitalRembourse;
-                expectedContent.AppendLine($"{mensualite.Numero};{capitalRembourseCumulatif.ToString("F2", CultureInfo.InvariantCulture)};{mensualite.CapitalRestantDu.ToString("F2", CultureInfo.InvariantCulture)};{mensualite.Interet.ToString("F2", CultureInfo.InvariantCulture)};{mensualite.MensualiteTotal.ToString("F2", CultureInfo.InvariantCulture)};{coutTotalInitial.ToString("F2", CultureInfo.InvariantCulture)}");
+                coutTotal -= mensualite.MensualiteTotal;
+
+                expectedContent.AppendLine($"{mensualite.Numero};{capitalRembourseCumulatif.ToString("F2", CultureInfo.InvariantCulture)};{mensualite.CapitalRestantDu.ToString("F2", CultureInfo.InvariantCulture)};{mensualite.Interet.ToString("F2", CultureInfo.InvariantCulture)};{mensualite.MensualiteTotal.ToString("F2", CultureInfo.InvariantCulture)};{coutTotal.ToString("F2", CultureInfo.InvariantCulture)}");
             }
 
             createurCSV.GenererCSV(filePath, mensualites, coutTotalInitial);
